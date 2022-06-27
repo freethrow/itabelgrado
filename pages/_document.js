13a22eddd8b8932809454034a64c75aa1f4b6f68
freethrow <aleksendric@gmail.com>
2022-06-27 15:36:03 +0200

    added clicky analytics

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,10 @@ class MyDocument extends Document {
 
       <script async src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"
    integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
-   crossOrigin=""></script>
+   crossOrigin="true"></script>
+
+<script async src="//static.getclicky.com/101371268.js"></script>
+
 
       </Head>
         <body>
